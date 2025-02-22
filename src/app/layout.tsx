51dc7bb7a75e53ce6d6878ee36b072da2fc9c744
Steven <stevenpalominodesign@gmail.com>
2025-02-22 15:45:38 -0600

    snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2223364264861174"
+     crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
